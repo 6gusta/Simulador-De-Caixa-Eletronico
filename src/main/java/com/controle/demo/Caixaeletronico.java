a2411efe37fa3d764673a2f25a636634900e67e9
gustavo <luiz.carvalho7110@sounidesc.com.br>
2024-03-27 01:41:28 -0300

    atualização final do sistema do back end

--- src/main/java/com/controle/demo/Caixaeletronico.java
+++ src/main/java/com/controle/demo/Caixaeletronico.java
@@ -5,88 +5,116 @@ import java.util.Scanner;
 public class Caixaeletronico {
 
     public void Caixaeletronic(Numerodaconta conta) {
-        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=--=");
-        System.out.println(" CAIXA 24 HORAS ");
-        System.out.println("=-=-=-=-=-=-=-==-=--=-======");
-
         Scanner scanner = new Scanner(System.in);
-        int numeroContaDigitado;
-        int tentativasSenha = 0; 
-        // Variável para controlar as tentativas de senha
 
+        while (true) {
+            System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=--=");
+            System.out.println(" CAIXA 24 HORAS ");
+            System.out.println("=-=-=-=-=-=-=-==-=--=-======");
 
-        do {
-            System.out.print("Digite o número da conta: ");
-            String input = scanner.nextLine();
-            if (input.matches("\\d+")) { // Verifica se a entrada contém apenas dígitos
-                numeroContaDigitado = Integer.parseInt(input);
-                break; // Sai do loop se a entrada for um número válido
-            } else {
-                System.out.println("Entrada inválida. Digite apenas números para o número da conta.");
-            }
-        } 
-        while (true);
+            int numeroContaDigitado;
+            int tentativasSenha = 0; 
 
-        int numeroContaArmazenado = conta.getnumcartao();
-
-        System.out.println("Por favor, aguarde enquanto validamos seu cartão...");
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    
-        
-
-        if (numeroContaDigitado == numeroContaArmazenado) {
-            System.out.println("A validação do cartão foi concluída. Próxima informação...");
-            System.out.println("Olá " + conta.getnome() + ". Para continuar, aperte qualquer tecla: ");
-            System.out.println("Você está no banco " + conta.getTipodeconta() + " na sua conta " + conta.getPouc());
-
-            String numcolocado = scanner.nextLine();
-
-            System.out.println("Escolha uma opção:");
-            System.out.println("1. SACAR");
-            System.out.println("2. SALDO");
-            int opcao = Integer.parseInt(scanner.nextLine());
-            if (opcao == 1) {
-                System.out.print("Informe o valor a ser sacado: ");
-                int saque = Integer.parseInt(scanner.nextLine());
-                System.out.println("Digite sua senha: ");
-
-                while (tentativasSenha < 3) { // Loop para lidar com as tentativas de senha
-                    String senhaDigitada = scanner.nextLine(); // Recebe a senha como uma string
-                    String senhaArmazenada = String.valueOf(conta.getSenha()); // Converte a senha armazenada para uma string
-
-                    if (senhaDigitada.equals(senhaArmazenada)) {
-                        // Senha correta, verificar o saldo
-                        if (saque <= conta.getValor()) {
-                            // Saldo suficiente, realizar saque
-                            conta.setValor(conta.getValor() - saque);
-                            System.out.println("Saque realizado com sucesso. Novo saldo: " + conta.getValor());
-                        } else {
-                            // Saldo insuficiente
-                            System.out.println("Saldo insuficiente.");
-                        }
-                        break; // Sai do loop de tentativa de senha
-                    } else {
-                        // Senha incorreta
-                        System.out.println("Senha inválida.");
-                        tentativasSenha++;
-                        if (tentativasSenha >= 3) { // Se as tentativas de senha excederem 3, bloqueie o cartão
-                            System.out.println("Muitas tentativas inválidas. Seu cartão foi bloqueado.");
-                            return; // Sai do método
-                        }
-                    }
+            do {
+                System.out.print("Digite o número da conta: ");
+                String input = scanner.nextLine();
+                if (input.matches("\\d+")) { 
+                    numeroContaDigitado = Integer.parseInt(input);
+                    break; 
+                } else {
+                    System.out.println("Entrada inválida. Digite apenas números para o número da conta.");
                 }
-            } else if (opcao == 2) {
-                System.out.println("Saldo disponível: " + conta.getValor());
-            } else {
-                System.out.println("Opção inválida.");
+            } while (true);
+
+            int numeroContaArmazenado = conta.getnumcartao();
+
+            System.out.println("Por favor, aguarde enquanto validamos seu cartão...");
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        } else {
-            System.out.println("Número da conta inválido.");
+
+            if (numeroContaDigitado == numeroContaArmazenado) {
+                System.out.println("A validação do cartão foi concluída. Próxima informação...");
+                System.out.println("Olá " + conta.getnome() + ". Para continuar, aperte qualquer tecla: ");
+                System.out.println("Você está no banco " + conta.getTipodeconta() + " na sua conta " + conta.getPouc());
+
+                String numcolocado = scanner.nextLine();
+
+                System.out.println("Escolha uma opção:");
+                System.out.println("1. SACAR");
+                System.out.println("2. SALDO");
+                System.out.println("3. DEPÓSITO");
+                System.out.println("4. TRANSFERÊNCIA");
+                int opcao = Integer.parseInt(scanner.nextLine());
+
+                switch (opcao) {
+                    case 1:
+                        System.out.print("Informe o valor a ser sacado: ");
+                        int saque = Integer.parseInt(scanner.nextLine());
+                        System.out.println("Digite sua senha: ");
+
+                        while (tentativasSenha < 3) { 
+                            String senhaDigitada = scanner.nextLine(); 
+                            String senhaArmazenada = String.valueOf(conta.getSenha()); 
+
+                            if (senhaDigitada.equals(senhaArmazenada)) {
+                                if (saque <= conta.getValor()) {
+                                    conta.setValor(conta.getValor() - saque);
+                                    System.out.println("Saque realizado com sucesso. Novo saldo: " + conta.getValor());
+                                } else {
+                                    System.out.println("Saldo insuficiente.");
+                                }
+                                break; 
+                            } else {
+                                System.out.println("Senha incorreta.");
+                                tentativasSenha++;
+                                if (tentativasSenha >= 3) { 
+                                    System.out.println("Muitas tentativas inválidas. Seu cartão foi bloqueado.");
+                                    return; 
+                                }
+                            }
+                        }
+                        break;
+
+                    case 2:
+                        System.out.println("Saldo disponível: " + conta.getValor());
+                        break;
+
+                    case 3:
+                        System.out.print("Informe o valor a ser depositado: ");
+                        int deposito = Integer.parseInt(scanner.nextLine());
+                        System.out.println("Digite sua senha: ");
+                        String senhaDigitada = scanner.nextLine();
+                        String senhaArmazenada = String.valueOf(conta.getSenha());
+                        if (senhaDigitada.equals(senhaArmazenada)) {
+                            conta.setValor(conta.getValor() + deposito);
+                            System.out.println("Depósito realizado com sucesso. Novo saldo: " + conta.getValor());
+                        } else {
+                            System.out.println("Senha incorreta. Operação cancelada.");
+                        }
+                        break;
+
+                    case 4:
+                        System.out.println("No momento essa opção se encontra indisponível.");
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida.");
+                        break;
+                }
+
+            } else {
+                System.out.println("Número da conta inválido.");
+            }
+
+            System.out.println("Deseja realizar outra operação? (S/N)");
+            String continuar = scanner.nextLine();
+            if (!continuar.equalsIgnoreCase("S")) {
+                break;
+            }
         }
     }
 }
