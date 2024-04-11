89e2eccc2f29957967efdb3111fb28945a4fb90b
gustavo <luiz.carvalho7110@sounidesc.com.br>
2024-04-11 03:02:22 -0300

    atualização do arquivo opcao.html

--- src/main/java/com/controle/demo/Controller/ControllerController.java
+++ src/main/java/com/controle/demo/Controller/ControllerController.java
@@ -1,9 +1,12 @@
 package com.controle.demo.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
@@ -21,21 +24,27 @@ public class ControllerController {
         return "index.html";
     }
     
-
-        @RestController
+@RestController
 public class VerificarContaController {
 
     // Endpoint para verificar a conta
     @GetMapping("/api/verificar-conta")
-    public String verificarConta(@RequestParam String numeroConta) {
+    @ResponseBody
+    public ResponseEntity<String> verificarConta(@RequestParam String numeroConta) {
         // Lógica para verificar se o número da conta é válido
-        if ("44072853".equals(numeroConta) || "44028922".equals(numeroConta)) {
-            return "Conta válida";
+        if ("44072853".equals(numeroConta)) {
+            String htmlContent = "<h1>Conta válida - Tipo A</h1>";
+            return new ResponseEntity<>(htmlContent, HttpStatus.OK);
+        } else if ("44028922".equals(numeroConta)) {
+            String htmlContent = "<h1>Conta válida - Tipo B</h1>";
+            return new ResponseEntity<>(htmlContent, HttpStatus.OK);
         } else {
-            return "Número da conta inválido";
+            String htmlContent = "<h1>Número da conta inválido</h1>";
+            return new ResponseEntity<>(htmlContent, HttpStatus.BAD_REQUEST);
         }
     }
 }
+
 
     
     
